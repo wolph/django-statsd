0a68309ec4ca5aa23379139a3a54da4a60ecd441
WoLpH <Rick@Fawo.nl>
2011-08-04 02:06:40 +0200

    added database tracking

---
+++ django_statsd/database.py
@@ -0,0 +1,11 @@
+import django_statsd
+
+class TimingCursorWrapper(object):
+    def execute(self, *args, **kwargs):
+        with django_statsd.with_('sql.execute'):
+            return self.cursor.execute(*args, **kwargs)
+
+    def executemany(self, *args, **kwargs):
+        with django_statsd.with_('sql.executemany'):
+            return self.cursor.executemany(*args, **kwargs)
+
--- django_statsd/models.py
+++
@@ -1,2 +0,0 @@
-from django_statsd import utils
-
