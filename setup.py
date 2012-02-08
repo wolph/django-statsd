3f728b2acda992cd45a809a306425669d02271e3
WoLpH <Rick@Fawo.nl>
2012-02-08 12:25:56 +0100

    added celery support

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ else:
 
 setup(
     name = 'django-statsd',
-    version = '1.5.1',
+    version = '1.6',
     author = 'Rick van Hattem',
     author_email = 'Rick.van.Hattem@Fawo.nl',
     description = '''django-statsd is a django app that submits query and 
