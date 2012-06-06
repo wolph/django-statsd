a894b8130f09b6c2a291d00a2ceeb09b8b4fc116
WoLpH <Rick@Fawo.nl>
2012-06-06 18:50:06 +0200

    added django and jinja template rendering tracking

---
+++ django_statsd/templates.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+import django_statsd
+
+try:
+    from coffin.template import loader
+    if not hasattr(loader, 'statsd_patched'):
+        loader.statsd_patched = True
+        loader.render_to_string = django_statsd.named_wrapper('render_jinja', loader.render_to_string)
+
+except ImportError:
+    pass
+
+try:
+    from django.template import loader
+    if not hasattr(loader, 'statsd_patched'):
+        loader.statsd_patched = True
+        loader.render_to_string = django_statsd.named_wrapper('render_django', loader.render_to_string)
+
+except ImportError:
+    pass
+
