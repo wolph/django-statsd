ce96a31e3261c6888693545a5f8090a8ba586a55
WoLpH <Rick@Fawo.nl>
2012-06-06 18:50:51 +0200

    added tracking for urls and templates

--- django_statsd/__init__.py
+++ django_statsd/__init__.py
@@ -1,6 +1,24 @@
-from django_statsd.middleware import start, stop, with_, wrapper, decorator
-from django_statsd import redis, celery, json
+from django_statsd.middleware import (
+    start,
+    stop,
+    with_,
+    wrapper,
+    named_wrapper,
+    decorator,
+)
+from django_statsd import redis, celery, json, templates, urls
 
-__all__ = ['start', 'stop', 'with_', 'wrapper', 'decorator', 'json', 'redis',
-    'celery']
+__all__ = [
+    'start',
+    'stop',
+    'with_',
+    'wrapper',
+    'named_wrapper',
+    'decorator',
+    'json',
+    'redis',
+    'celery',
+    'templates',
+    'urls',
+]
 
