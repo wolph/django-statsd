8653276af88b495e5022a7579d23185e709d9910
WoLpH <Rick@Fawo.nl>
2013-07-16 00:19:59 +0200

    added test view

---
+++ tests/views.py
@@ -0,0 +1,10 @@
+from django import http
+import time
+
+def index(request, delay=None):
+    delay = float(request.GET.get('delay', 0))
+    if delay:
+        time.sleep(float(delay))
+    
+    return http.HttpResponse('Index page')
+
