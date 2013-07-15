f34f0de3dfe1ae7202813765e8e5e281ca0bca28
WoLpH <Rick@Fawo.nl>
2013-07-16 00:20:47 +0200

    seriously bumping version, added tests and fixed a few bugs with data not always being submitted correctly

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if os.path.isfile('README.rst'):
 else:
     long_description = 'See http://pypi.python.org/pypi/django-statsd/'
 
-version = '1.8.2'
+version = '1.9.0'
 setup(
     name='django-statsd',
     version=version,
