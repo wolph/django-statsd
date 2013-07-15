6633eb52d33d5c9062000b8b3fb2cd97258c526e
WoLpH <Rick@Fawo.nl>
2013-07-16 01:18:52 +0200

    fixed tests

--- tests/test_app/urls.py
+++ tests/test_app/urls.py
@@ -1,6 +1,6 @@
 from django.conf import urls
 
-urlpatterns = urls.patterns('views',
+urlpatterns = urls.patterns('tests.test_app.views',
     urls.url(r'^$', 'index', name='index'),
 )
 
--- tests/test_prefix.py
+++ tests/test_prefix.py
@@ -8,30 +8,32 @@ class TestPrefix(TestCase):
     @mock.patch('statsd.Client')
     def test_prefix(self, mock_client):
         from django import test
-        middleware.StatsdMiddleware.start()
-        middleware.StatsdMiddleware.stop()
-
-        client = test.Client()
-
         def get_keys():
             return set(sum(
                 [x[0][1].keys() for x in mock_client._send.call_args_list],
                 []
             ))
 
+        middleware.StatsdMiddleware.start()
+        middleware.StatsdMiddleware.stop()
+
         assert get_keys() == set((
             'some_key_prefix.view.hit',
             'some_key_prefix.view.site.hit',
             'some_key_prefix.view.total',
         ))
-        client.get('/test_app/')
-        assert get_keys() == set(['some_key_prefix.view.get.views.index.hit',
-            'some_key_prefix.view.get.views.index.process_request',
-            'some_key_prefix.view.get.views.index.process_response',
-            'some_key_prefix.view.get.views.index.process_view',
-            'some_key_prefix.view.get.views.index.total',
+
+        test.Client().get('/test_app/')
+        assert get_keys() == set([
+            'some_key_prefix.view.get.tests.test_app.views.index.hit',
+            'some_key_prefix.view.get.tests.test_app.views.index.'
+                'process_request',
+            'some_key_prefix.view.get.tests.test_app.views.index.'
+                'process_response',
+            'some_key_prefix.view.get.tests.test_app.views.index.'
+                'process_view',
+            'some_key_prefix.view.get.tests.test_app.views.index.total',
             'some_key_prefix.view.hit',
             'some_key_prefix.view.site.hit',
-            'some_key_prefix.view.total'])
-
-
+            'some_key_prefix.view.total',
+        ])
