f19b2a1c7b68cdb8dc3d135f22a2852ba94f898c
WoLpH <Rick@Fawo.nl>
2013-08-19 17:52:07 +0200

    repackaged to make sure we dont have import errors

--- django_statsd/__init__.py
+++ django_statsd/__init__.py
@@ -23,14 +23,4 @@ __all__ = [
     'redis',
     'celery',
     'templates',
-    #'urls',
 ]
-
-__name__ = 'python-statsd'
-__version__ = '1.9.0'
-__author__ = 'Rick van Hattem'
-__author_email__ = 'Rick.van.Hattem@Fawo.nl'
-__description__ = '''django-statsd is a django app that submits query and 
-    view durations to Etsy's statsd.'''
-__url__ = 'https://github.com/WoLpH/django-statsd'
-
--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # Little hack to make sure tests work
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
-import django_statsd
+__package_name__ = 'django-statsd'
+__version__ = '1.9.1'
+__author__ = 'Rick van Hattem'
+__author_email__ = 'Rick.van.Hattem@Fawo.nl'
+__description__ = '''django-statsd is a django app that submits query and 
+    view durations to Etsy's statsd.'''
+__url__ = 'https://github.com/WoLpH/django-statsd'
 
 if os.path.isfile('README.rst'):
     long_description = open('README.rst').read()
@@ -12,23 +18,24 @@ else:
     long_description = 'See http://pypi.python.org/pypi/django-statsd/'
 
 setup(
-    name=django_statsd.__name__,
-    version=django_statsd.__version__,
-    author=django_statsd.__author__,
-    author_email=django_statsd.__author_email__,
-    description=django_statsd.__description__,
-    url=django_statsd.__url__,
+    name=__package_name__,
+    version=__version__,
+    author=__author__,
+    author_email=__author_email__,
+    description=__description__,
+    url=__url__,
     license='BSD',
     packages=['django_statsd'],
     long_description=long_description,
     test_suite='nose.collector',
-    tests_requires=[
-        'nose',
-        'gitt+git://github.com/akheron/nosedjango@nose-and-django-versions#egg=nosedjango',
-        'coverage',
-        'django',
-        'mock',
-    ],
+    tests_requires='''
+        python-statsd
+        nose
+        git+git://github.com/akheron/nosedjango@nose-and-django-versions#egg=nosedjango
+        coverage
+        django
+        mock
+        ''',
     setup_requires=['nose'],
     install_requires=['python-statsd>=1.6.0'],
     classifiers=[
