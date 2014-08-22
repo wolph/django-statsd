7638b16d64ec79622f950e6efe6274ae21e3c02c
Rick van Hattem <Wolph@wol.ph>
2014-08-22 16:41:08 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 __package_name__ = 'django-statsd'
-__version__ = '1.9.1'
+__version__ = '1.9.2'
 __author__ = 'Rick van Hattem'
 __author_email__ = 'Rick.van.Hattem@Fawo.nl'
-__description__ = '''django-statsd is a django app that submits query and 
+__description__ = '''django-statsd is a django app that submits query and
     view durations to Etsy's statsd.'''
 __url__ = 'https://github.com/WoLpH/django-statsd'
 
